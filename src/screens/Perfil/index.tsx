bc94c44be7b93e32170e22612a3b8861ec75b0c7
pedromathias <pedrohmathias10@gmail.com>
2022-11-21 22:38:50 -0300

    correção de erro

--- src/screens/Perfil/index.tsx
+++ src/screens/Perfil/index.tsx
@@ -10,7 +10,7 @@ export const Perfil = () => {
         <View style={styles.container}>
             <Text style={styles.titulo}>LOLJA</Text>
             <View style={styles.content}>
-            <Image style={styles.imageprofile} source={ProfileImage}></Image>
+            <Image style={styles.imageprofile} source={ProfileImage} />
             <TouchableOpacity style={styles.editprofilebutton}><Text style={styles.editprofiletext}>Editar Perfil</Text></TouchableOpacity>
             <View style={{width:'100%', alignItems:'center', marginTop:100}}>
             <TouchableOpacity style={styles.optionsprofile}><FontAwesome5 style={styles.iconbutton} name="shopping-basket" size={28} color="black" /><Text style={{color:'white', fontSize:20}}>Ver Pedidos</Text></TouchableOpacity>
