a44270096450ce089f4f785c620dd5431d519208
pedromathias <pedrohmathias10@gmail.com>
2022-11-21 22:21:14 -0300

    criação da pagina perfil

--- App.js
+++ App.js
@@ -2,11 +2,12 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, TextInput, View } from 'react-native';
 import { Cadastro } from './src/screens/Cadastro';
 import { Login } from './src/screens/Login';
+import { Perfil } from './src/screens/Perfil';
 
 export default function App() {
   return (
     <View>
-      <Cadastro />
+      <Perfil />
     </View>
   );
 }
