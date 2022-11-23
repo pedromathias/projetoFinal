4420d7db0e58450c9a3e6f30604752fe6f62cb22
MatheusCarius <matheus_julianoc@hotmail.com>
2022-11-22 22:08:36 -0300

    Adicionando Routes ao app.tsx

--- App.tsx
+++ App.tsx
@@ -1,17 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, TextInput, View } from 'react-native';
 import { ProvedorCarrinho } from './src/Context/CarrinhoContexto';
-import { Cadastro } from './src/screens/Cadastro';
-import { Carrinho } from './src/screens/Carrinho';
-import { Home } from './src/screens/Home';
-import { Login } from './src/screens/Login';
-import { Perfil } from './src/screens/Perfil';
+import { Routes } from './src/routes';
 
 export default function App() {
   return (
     <ProvedorCarrinho>
       <StatusBar style={'light'} />
-      <Carrinho />
+      <Routes />
     </ProvedorCarrinho>
   );
 }
