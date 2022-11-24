78f59e96f1c26d356228ec3065d60ccaaae409b4
pedromathias <pedrohmathias10@gmail.com>
2022-11-24 02:15:16 -0300

    estilização da tela de produto

--- src/screens/Produto/styles.ts
+++ src/screens/Produto/styles.ts
@@ -29,26 +29,43 @@ content: {
 },
 
 title:{
+    textAlign:'center',
     fontSize: 50,
     marginTop: 30
 },
 descricao:{
-    fontSize:24,
+    fontSize:20,
     marginTop: 20,
-    marginBottom:50
+    marginBottom:50,
+    opacity:0.5,
+    textAlign:'center'
 },
 preco:{
     fontSize: 30,
+    marginBottom:50
+},
+
+categoria:{
+    fontSize: 20,
+    marginLeft:15,
     marginBottom:20
 },
+
+quantidade:{
+    fontSize: 20,
+    marginLeft:15,
+    marginBottom:20
+},
+
 img:{
     width: 320,
-    heigh: 320
+    height: 320,
+    borderRadius:100,
 },
 image:{
-    backgroundColor: "#111",
     marginTop: 50,
-    height: 300
+    height: 300,
+    alignItems:'center'
     
 },
 botaoCarrinho:{
